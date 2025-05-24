8420137ef0aec5ba2b5ca0113e09effc7984f2a8
nvvoitov <nvvoitov@gmail.com>
2025-05-24 14:00:50 +0300

    Fix: Update embeddings init file

--- causal_econ/embeddings/__init__.py
+++ causal_econ/embeddings/__init__.py
@@ -1,3 +1,3 @@
-from .e2v_embeddings import Economic2Vec, run_e2v_analysis, generate_geminis
+from .e2v import Economic2Vec, run_e2v_analysis, generate_geminis
 
 __all__ = ['Economic2Vec', 'run_e2v_analysis', 'generate_geminis']
